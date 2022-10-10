deb05c940806d076eaa3d3afecb99777f0bc6672
KusnirekAlexis <83014109+KusnirekAlexis@users.noreply.github.com>
2022-10-10 15:39:58 +0200

    Debut de projet

---
+++ Source/JeuTourParTour/JeuTourParTour.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JeuTourParTour.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JeuTourParTour, "JeuTourParTour" );
---
+++ Source/JeuTourParTour/JeuTourParTourGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JeuTourParTourGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JEUTOURPARTOUR_API AJeuTourParTourGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
